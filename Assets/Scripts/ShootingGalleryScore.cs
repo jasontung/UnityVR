246293060693006cf40bcaae466db067b6e83a19
jasontung2214 <jasontung2214@gmail.com>
2017-11-24 20:15:31 +0800

    1. delete complete scripts

---
+++ Assets/Scripts/ShootingGalleryScore.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.UI;
+using VRStandardAssets.Common;
+
+// This script displays the player's score during the
+// shooter scenes.
+public class ShootingGalleryScore : MonoBehaviour
+{
+    [SerializeField] private Text m_ScoreText;
+
+
+    private void Update()
+    {
+        m_ScoreText.text = SessionData.Score.ToString();
+    }
+}
