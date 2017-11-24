3c929dd8252c2aa441ab690ecf12c1e704f39ca3
jasontung2214 <jasontung2214@gmail.com>
2017-11-24 20:01:49 +0800

    1. reset to initial project

--- Assets/Scripts/ShootingGunController.cs
+++ Assets/Scripts/ShootingGunController.cs
@@ -1,76 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using VRStandardAssets.Utils;
-using UnityEngine.VR;
-public class ShootingGunController : MonoBehaviour {
-    public LineRenderer gunFlare;
-    public VRInput vrInput;
-    public GameManager gameManager;
-    public VREyeRaycaster eyeRaycaster;
-    public AudioSource audioSource;
-    public Transform gunEnd;
-    public ParticleSystem flareParticles;
-    public Transform cameraTransform;
-    public Reticle reticle;
-    public Transform gunContainer;
-    public float defaultLineLength = 70f;
-    public float gunFlareVisibleSeconds = 0.07f;
-    public float damping = 0.5f;
-    public float gunContainerSmooth = 10f;
-    private const float dampingCoef = -20f;
-    private void Awake()
-    {
-        gunFlare.enabled = false;
-    }
-
-    private void OnEnable()
-    {
-        vrInput.OnDown += HandleDown;
-    }
-
-    private void OnDisable()
-    {
-        vrInput.OnDown -= HandleDown;
-    }
-
-    private void HandleDown()
-    {
-        if (gameManager.IsPlaying == false)
-            return;
-        ShootingTarget shootingTarget = eyeRaycaster.CurrentInteractible ? eyeRaycaster.CurrentInteractible.GetComponent<ShootingTarget>() : null;
-        Transform target = shootingTarget ? shootingTarget.transform : null;
-        StartCoroutine(Fire(target));
-    }
-    private IEnumerator Fire(Transform target)
-    {
-        audioSource.Play();
-        float lineLength = defaultLineLength;
-        if (target)
-            lineLength = Vector3.Distance(gunEnd.position, target.position);
-        flareParticles.Play();
-        gunFlare.enabled = true;
-        yield return StartCoroutine(MoveLineRenderer(lineLength));
-        gunFlare.enabled = false;
-    }
-
-    private IEnumerator MoveLineRenderer(float lineLength)
-    {
-        float timer = 0f;
-        while(timer < gunFlareVisibleSeconds)
-        {
-            gunFlare.SetPosition(0, gunEnd.position);
-            gunFlare.SetPosition(1, gunEnd.position + gunEnd.forward * lineLength);
-            yield return null;
-            timer += Time.deltaTime;
-        }
-    }
-
-    private void Update()
-    {
-        transform.rotation = Quaternion.Slerp(transform.rotation, InputTracking.GetLocalRotation(VRNode.Head), damping * (1 - Mathf.Exp(dampingCoef * Time.deltaTime)));
-        transform.position = cameraTransform.position;
-        Quaternion lookAtRotation = Quaternion.LookRotation(reticle.ReticleTransform.position - gunContainer.position);
-        gunContainer.rotation = Quaternion.Slerp(gunContainer.rotation, lookAtRotation, gunContainerSmooth * Time.deltaTime);
-    }
+//using VRStandardAssets.Utils;
+//using UnityEngine.VR;
+public class ShootingGunController : MonoBehaviour
+{
+   
 }
--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -1,45 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using VRStandardAssets.Utils;
-using VRStandardAssets.Common;
+//using VRStandardAssets.Utils;
+//using VRStandardAssets.Common;
 using UnityEngine.UI;
-public class UIController : MonoBehaviour {
-    public UIFader introUI;
-    public UIFader outroUI;
-    public UIFader playerUI;
-    public Text totalScore;
-    public Text highScore;
-
-	public IEnumerator ShowIntroUI()
-    {
-        yield return StartCoroutine(introUI.InteruptAndFadeIn());
-    }
-
-    public IEnumerator HideIntroUI()
-    {
-        yield return StartCoroutine(introUI.InteruptAndFadeOut());
-    }
-
-    public IEnumerator ShowOutroUI()
-    {
-        totalScore.text = SessionData.Score.ToString();
-        highScore.text = SessionData.HighScore.ToString();
-        yield return StartCoroutine(outroUI.InteruptAndFadeIn());
-    }
-
-    public IEnumerator HideOutroUI()
-    {
-        yield return StartCoroutine(outroUI.InteruptAndFadeOut());
-    }
-
-    public IEnumerator ShowPlayerUI()
-    {
-        yield return StartCoroutine(playerUI.InteruptAndFadeIn());
-    }
-
-    public IEnumerator HidePlayerUI()
-    {
-        yield return StartCoroutine(playerUI.InteruptAndFadeOut());
-    }
+public class UIController : MonoBehaviour
+{
+   
 }
